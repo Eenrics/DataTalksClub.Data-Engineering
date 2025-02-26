e46eb8f44b2a43d4c1e7e36b78acbd7f5ecfcecf
Abenezer Eshetie <ebenezeresh525@gmail.com>
2025-02-26 14:42:28 +0300

    add initial models, macros, and seeds for NYC taxi rides analytics

--- 04-analytics-engineering/scripts/script.sh
+++ 04-analytics-engineering/scripts/script.sh
@@ -2,3 +2,6 @@ python3 -m venv env
 source env/bin/activate
 alias env_dbt='source /Users/mac/data-engineering/DataTalksClub.Data-Engineering/04-analytics-engineering/env/bin/activate'
  python -m pip install dbt-core dbt-postgres
+
+ dbt init
+ dbt debug
