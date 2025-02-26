c79bd8d9b30040281f6c4934abc9794741b78dda
Abenezer Eshetie <ebenezeresh525@gmail.com>
2025-02-26 14:01:44 +0300

    dbt init

---
+++ 04-analytics-engineering/scripts/script.sh
@@ -0,0 +1,4 @@
+python3 -m venv env    
+source env/bin/activate
+alias env_dbt='source /Users/mac/data-engineering/DataTalksClub.Data-Engineering/04-analytics-engineering/env/bin/activate'
+ python -m pip install dbt-core dbt-postgres
